de9fafe83d7494ff82f723ab47c810e72c82d43e
sa-rajput <101544549+sa-rajput@users.noreply.github.com>
2025-11-19 14:37:52 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,27 +1,4 @@
-# outputs.tf (root)
-# Root-level conveniences that aggregate module outputs
-
-output "vpc_name" {
-  description = "VPC name from networking module"
-  value       = module.networking.network_name
-}
-
-output "subnet_self_link" {
-  description = "Subnet self_link"
-  value       = module.networking.subnet_self_link
-}
-
-output "gke_cluster_name" {
-  description = "GKE cluster name"
-  value       = module.gke.cluster_name
-}
-
-output "gke_endpoint" {
-  description = "GKE endpoint (control plane)"
-  value       = module.gke.endpoint
-}
-
-output "all_node_pool_names" {
-  description = "List of node pool names"
-  value       = module.node_pools.node_pool_names
-}
+output "gke_endpoint" { value = module.gke.endpoint }
+output "gke_cluster_name" { value = module.gke.name }
+output "network_self_link" { value = module.networking.network_self_link }
+output "subnet_self_link" { value = module.networking.subnet_self_link }
