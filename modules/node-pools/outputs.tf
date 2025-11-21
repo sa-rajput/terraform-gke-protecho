0efcfcac9ee745d3c14f6a66986f33a73f625106
sa-rajput <101544549+sa-rajput@users.noreply.github.com>
2025-11-21 13:34:41 +0530

    Delete modules/node-pools directory

--- modules/node-pools/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "node_pools" {
-  value = [
-    google_container_node_pool.admin.name,
-    google_container_node_pool.tidb.name,
-    google_container_node_pool.pd.name,
-    google_container_node_pool.tikv.name,
-    google_container_node_pool.bignode.name,
-  ]
-}
